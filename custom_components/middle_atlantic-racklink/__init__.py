486540ddc14fa0999a1cf7d4ed1d5104c1da0b81
mckay115 <zacharylisko@gmail.com>
2024-07-14 08:38:23 -0500

    Update __init__.py

--- custom_components/middle_atlantic-racklink/__init__.py
+++ custom_components/middle_atlantic-racklink/__init__.py
@@ -2,19 +2,24 @@ import asyncio
 import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 from .const import DOMAIN
 from .racklink_controller import RacklinkController
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = ["switch", "sensor", "binary_sensor"]
+PLATFORMS = [Platform.SWITCH, Platform.SENSOR, Platform.BINARY_SENSOR]
 
-async def async_setup(hass: HomeAssistant, config: dict):
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the RackLink component."""
     hass.data[DOMAIN] = {}
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up RackLink from a config entry."""
     controller = RacklinkController(
         hass,
@@ -28,23 +33,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data[DOMAIN][entry.entry_id] = controller
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, platform)
-                for platform in PLATFORMS
-            ]
-        )
-    )
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     if unload_ok:
         controller = hass.data[DOMAIN].pop(entry.entry_id)
