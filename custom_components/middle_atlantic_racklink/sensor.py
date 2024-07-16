8808afdd69ffa2f648a77832e4ed0bf26fe09481
mckay115 <zacharylisko@gmail.com>
2024-07-15 20:27:07 -0500

    Update sensor.py

--- custom_components/middle_atlantic_racklink/sensor.py
+++ custom_components/middle_atlantic_racklink/sensor.py
@@ -2,10 +2,15 @@ from homeassistant.helpers.entity import Entity
 from .const import DOMAIN
 
 SENSOR_TYPES = {
-    0x52: {"name": "Voltage", "unit": "V"},
-    0x54: {"name": "Current", "unit": "A"},
-    0x56: {"name": "Power", "unit": "W"},
-    0x55: {"name": "Temperature", "unit": "°F"},
+    0x50: {"name": "Kilowatt Hours", "unit": "kWh", "icon": "mdi:power-plug"},
+    0x51: {"name": "Peak Voltage", "unit": "V", "icon": "mdi:flash"},
+    0x52: {"name": "RMS Voltage", "unit": "V", "icon": "mdi:flash"},
+    0x53: {"name": "Peak Load", "unit": "A", "icon": "mdi:current-ac"},
+    0x54: {"name": "RMS Load", "unit": "A", "icon": "mdi:current-ac"},
+    0x55: {"name": "Temperature", "unit": "°F", "icon": "mdi:thermometer"},
+    0x56: {"name": "Wattage", "unit": "W", "icon": "mdi:power-plug"},
+    0x57: {"name": "Power Factor", "unit": None, "icon": "mdi:angle-acute"},
+    0x58: {"name": "Thermal Load", "unit": "BTU", "icon": "mdi:radiator"},
 }
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -21,6 +26,7 @@ class RacklinkSensor(Entity):
         self._sensor_type = sensor_type
         self._name = sensor_info["name"]
         self._unit = sensor_info["unit"]
+        self._icon = sensor_info["icon"]
         self._state = None
 
     @property
@@ -34,6 +40,10 @@ class RacklinkSensor(Entity):
     @property
     def unit_of_measurement(self):
         return self._unit
+
+    @property
+    def icon(self):
+        return self._icon
 
     async def async_update(self):
         self._state = await self._controller.get_sensor_value(self._sensor_type)
